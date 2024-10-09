6e818cd33290f7f55c97f93c91f465a425b5e5a7
HSK <haakonsk@live.no>
2024-10-09 20:09:47 +0200

    Added checkboxes on shopping cart

--- lib/pages/shopping_cart.dart
+++ lib/pages/shopping_cart.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_app/providers/shopping_cart_provider.dart';
 
-class ShoppingCartScreen extends ConsumerWidget {
+class ShoppingCartScreen extends ConsumerStatefulWidget {
   const ShoppingCartScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  _ShoppingCartScreenState createState() => _ShoppingCartScreenState();
+}
+
+class _ShoppingCartScreenState extends ConsumerState<ShoppingCartScreen> {
+  final Map<String, bool> _checkedItems = {};
+
+  @override
+  Widget build(BuildContext context) {
     final shoppingCart = ref.watch(shoppingCartProvider);
 
     return Scaffold(
@@ -16,12 +23,32 @@ class ShoppingCartScreen extends ConsumerWidget {
       body: ListView.builder(
         itemCount: shoppingCart.length,
         itemBuilder: (ctx, index) {
+          final item = shoppingCart[index];
+          final isChecked = _checkedItems[item] ?? false;
+
           return ListTile(
-            title: Text(shoppingCart[index]),
+            leading: Checkbox(
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  _checkedItems[item] = value ?? false;
+                });
+              },
+            ),
+            title: Text(
+              item,
+              style: TextStyle(
+                decoration: isChecked ? TextDecoration.lineThrough : TextDecoration.none,
+                color: isChecked ? Colors.grey : Colors.white,
+              ),
+            ),
             trailing: IconButton(
               icon: const Icon(Icons.delete),
               onPressed: () {
-                ref.read(shoppingCartProvider.notifier).removeIngredient(shoppingCart[index]);
+                ref.read(shoppingCartProvider.notifier).removeIngredient(item);
+                setState(() {
+                  _checkedItems.remove(item);
+                });
               },
             ),
           );
