a6eca083c71d2aa03a9466291ee80a9077b8bd8a
HSK <haakonsk@live.no>
2024-10-09 18:35:25 +0200

    Refactored main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:meals_app/pages/tabs.dart';
-
-// Define the theme for the application.
-final theme = ThemeData(
-  useMaterial3: true, // Enable Material 3 design.
-  colorScheme: ColorScheme.fromSeed(
-    brightness: Brightness.dark, // Use a dark theme.
-    seedColor: const Color.fromARGB(255, 131, 57, 0), // Seed color for the color scheme.
-  ),
-  textTheme: GoogleFonts.latoTextTheme(), // Use Lato font for text.
-);
 
 void main() {
   runApp(
     const ProviderScope(
-      child: App(), // Root widget of the application.
+      child: App(),
     ),
+  );
+}
+
+// Define the theme for the application.
+// Used on evey screen.
+ThemeData buildTheme() {
+  return ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      brightness: Brightness.dark,
+      seedColor: const Color.fromARGB(255, 174, 92, 239),
+    ),
+    textTheme: GoogleFonts.latoTextTheme(),
   );
 }
 
@@ -29,9 +31,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: theme, // Apply the defined theme.
-      home: const TabsScreen(), // Set the home screen to TabsScreen.
-      debugShowCheckedModeBanner: false, // Disable the debug banner.
+      theme: buildTheme(),
+      home: const TabsScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
